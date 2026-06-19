742b292a0ffb620bf431664b2595afa9d79a1d1f
heidyrodriguezgarcia9-collab <heidyrodriguezgarcia9@gmail.com>
2026-06-19 11:20:32 -0400

    Add PWA support: manifest, service worker, icons, offline caching

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'armonia-v1'
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.svg',
+  '/icon-512.svg',
+  '/favicon.svg',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS)
+    })
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((response) => {
+        if (response && response.status === 200) {
+          const clone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clone)
+          })
+        }
+        return response
+      }).catch(() => cached)
+
+      return cached || fetchPromise
+    })
+  )
+})
